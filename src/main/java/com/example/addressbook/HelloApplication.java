18660c6b99c4ffd82bdfb3373eb6d4ad4833bc2e
Alyce Kang <alycekang@gmail.com>
2025-04-20 14:01:04 +1000

    Added method to load quiz attempt results into line chart

--- src/main/java/com/example/addressbook/HelloApplication.java
+++ src/main/java/com/example/addressbook/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.addressbook;
 
+import com.example.addressbook.controller.ProgressReportController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -15,8 +16,16 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+//        stage.setTitle(TITLE);
+//        stage.setScene(scene);
+//        stage.show();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("progress-report-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+        ProgressReportController controller = fxmlLoader.getController();
+        controller.setLineChartData(ProgressReportController.generateQuizAttempts(5));
         stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/addressbook/controller/ProgressReportController.java
+++ src/main/java/com/example/addressbook/controller/ProgressReportController.java
@@ -1,4 +1,81 @@
 package com.example.addressbook.controller;
 
+import com.example.addressbook.model.Quiz;
+import com.example.addressbook.model.QuizAttempt;
+import com.example.addressbook.model.QuizQuestion;
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
+
+import java.util.concurrent.ThreadLocalRandom; // remove later
+
 public class ProgressReportController {
+    @FXML
+    private Label quizTopicLabel;
+    @FXML
+    private LineChart lineChart;
+    @FXML
+    private TextArea commentsArea;
+    @FXML
+    private Button backButton;
+
+    public void initialize() {
+        lineChart.setLegendVisible(false);
+        commentsArea.setText("""
+Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Eget dolor morbi non arcu risus. Quis lectus nulla at volutpat diam
+ut venenatis tellus in. Feugiat in fermentum posuere urna nec tincidunt
+praesent semper. Turpis tincidunt id aliquet risus feugiat in.
+Libero volutpat sed cras ornare. Facilisi morbi tempus iaculis urna.
+Bibendum est ultricies integer quis auctor. Eu augue ut lectus arcu.
+Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu.
+Gravida neque convallis a cras. Elit ut aliquam purus sit.
+Suspendisse ultrices gravida dictum fusce ut placerat.
+Integer feugiat scelerisque varius morbi enim nunc.
+Amet justo donec enim diam vulputate ut pharetra.
+Sapien pellentesque habitant morbi tristique.
+Lorem sed risus ultricies tristique nulla aliquet.
+Elementum nibh tellus molestie nunc non blandit massa.""");
+    }
+
+    @FXML
+    private void onBackButtonPressed() {
+        // go back to results page
+    }
+
+    public void setQuizTopicLabel(String topic) {
+        quizTopicLabel.setText("Progress Report: " + topic);
+    }
+
+    public void setCommentsAreaText(String comments) {
+        commentsArea.setText(comments);
+    }
+
+    public void setLineChartData(QuizAttempt[] quizAttempts) {
+        XYChart.Series series = new XYChart.Series();
+        for (int i = 0; i < quizAttempts.length; i++) {
+            series.getData().add(new XYChart.Data(i + 1, quizAttempts[i].getScorePercentage()));
+        }
+        lineChart.getData().add(series);
+    }
+
+    // generates an array of quiz attempts for testing -- delete later
+    public static QuizAttempt[] generateQuizAttempts(int numAttempts) {
+        QuizAttempt[] quizAttempts = new QuizAttempt[numAttempts];
+        Quiz quiz = new Quiz();
+        for (int i = 0; i < 4; i++) {
+            quiz.addQuestion(new QuizQuestion());
+        }
+        for (int i = 0; i < quizAttempts.length; i++) {
+            QuizAttempt attempt = new QuizAttempt(quiz);
+            for (int j = 0; j < attempt.getSelectedAnswers().length; j++) {
+                attempt.setSelectedAnswer(j, ThreadLocalRandom.current().nextInt(-1,
+                        attempt.getQuiz().getQuestion(j).getAnswersCount()));
+            }
+            quizAttempts[i] = attempt;
+        }
+        return quizAttempts;
+    }
 }
