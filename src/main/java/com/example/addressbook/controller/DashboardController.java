01e19842f5d051997ee931b54b4c3ebcaa609945
georgepanayides <george@thedigitalxx.com>
2025-04-23 16:59:11 +1000

    Removed some redundant code

--- src/main/java/com/example/addressbook/controller/DashboardController.java
+++ src/main/java/com/example/addressbook/controller/DashboardController.java
@@ -8,15 +8,6 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 public class DashboardController {
 
     @FXML
