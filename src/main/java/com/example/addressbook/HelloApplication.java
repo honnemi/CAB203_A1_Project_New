063e313def909a6c4f19ada12873be7d6793e614
Alyce Kang <alycekang@gmail.com>
2025-04-28 11:16:08 +1000

    Changed HelloApplication back to original

--- src/main/java/com/example/addressbook/HelloApplication.java
+++ src/main/java/com/example/addressbook/HelloApplication.java
@@ -16,16 +16,8 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
-//        stage.setTitle(TITLE);
-//        stage.setScene(scene);
-//        stage.show();
-
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("progress-report-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
-        ProgressReportController controller = fxmlLoader.getController();
-        controller.setLineChartData(ProgressReportController.generateQuizAttempts(5));
         stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
