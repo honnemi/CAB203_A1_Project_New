70e13a16ba0a82602b0f64a6dab88ba1803c0c3f
Alyce Kang <alycekang@gmail.com>
2025-04-30 16:43:28 +1000

    Added necessary imports to QuizAppAlertTest

--- src/test/QuizAppAlertTest.java
+++ src/test/QuizAppAlertTest.java
@@ -1,10 +1,17 @@
+import com.example.quizapp.model.quizAppAlert;
 
+import javafx.scene.control.Alert;
+// import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
 
 public class QuizAppAlertTest{
 
     @Test
     public void testQuizAppAlert() throws IOException {
-        QuizAppAlert quizAppAlert = new QuizAppAlert();
+        quizAppAlert quizAppAlert = new quizAppAlert();
         Alert alert = quizAppAlert.createAlert("Test Title", "Test Header", "Test Content");
 
         assertEquals(Alert.AlertType.WARNING, alert.getAlertType());
