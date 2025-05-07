c3c5690a35ec970c46fa07f5cfb050e6deb68b59
Angelica Siv <you@example.com>
2025-05-07 15:31:30 +1000

    Applied some styling to login and signup pages

--- src/main/java/com/example/quizapp/HelloApplication.java
+++ src/main/java/com/example/quizapp/HelloApplication.java
@@ -3,9 +3,11 @@ package com.example.quizapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     // Constants defining the window title and size
